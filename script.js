42f17e3dcec982fff30b4bfab7edbe84fe3b01ba
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2025-10-12 15:18:07 +0300

    Version 1 of til-JavaScript

---
+++ script.js
@@ -0,0 +1,34 @@
+// BRACKET NOTATION {
+
+// let greeting = "hello";
+// console.log(greeting[2]);
+
+// Example 2
+
+// let greeting = "hello";
+// let firstTwo = greeting[0] + greeting[1];
+// console.log(firstTwo);
+
+// TEMPLATE LITERALS
+
+// let name = "Alice";
+// console.log(`Hello, ${name}`);
+
+// Finding a substring in a string
+
+/*
+let sentence = "JavaScript is awesome!";
+let position = sentence.indexOf("awesome!");
+
+console.log(position); 
+
+*/
+
+// prompt() method
+
+// let age = prompt("How old are you?");
+// if (age !== null) {
+//   console.log("You are " + age + " years old.");
+// } else {
+//   console.log("User canceled the prompt.");
+// }
