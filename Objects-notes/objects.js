ddfa8ad8e482c8de8ba2d060feadfd67a10e9947
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2025-10-18 23:21:16 +0300

    Added examples for accessing nested objects and arrays

--- Objects-notes/objects.js
+++ Objects-notes/objects.js
@@ -12,6 +12,8 @@ console.log(person["name"]);
 
 */
 
+/*
+
 const person = {
   name: "Bob",
   age: 25,
@@ -22,3 +24,40 @@ const person = {
 const {job, city, ...remainingProperties} = person;
 
 console.log(remainingProperties);
+
+*/
+
+//  Accessing properties from nested objects and arrays
+
+/*
+
+const person = {
+  name: "Alice",
+  age: 30,
+  contact: {
+    email: "alice@example.com",
+    phone: {
+      home: "123-456-7890",
+      work: "098-765-4321",
+    },
+  },
+};
+
+console.log(person.contact.phone.work);
+
+//Bracket Notation
+console.log(person["contact"]["phone"]["work"]);
+
+*/
+// Accessing from a nested array
+
+const person = {
+  name: "Alice",
+  age: 30,
+  address: [
+    { type: "home", street: "123 Main St", city: "Anytown" },
+    { type: "work", street: "456 Market St", city: "Workville" },
+  ],
+};
+
+console.log(person.address[1].city);
