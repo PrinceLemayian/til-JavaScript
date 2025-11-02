60597597273eae9e58f3d10d8293b579fa55c3b3
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2025-11-03 01:58:39 +0300

    Added another example for the map() method

--- callBacks/map.js
+++ callBacks/map.js
@@ -1,3 +1,6 @@
+// Example 1
+
+/*
 const students = ["Spongebob", "Patrick", "Squidward", "Sandy"];
 const studentsUpper = students.map(upperCase);
 
@@ -5,4 +8,17 @@ console.log(studentsUpper);
 
 function upperCase(element) {
   return element.toUpperCase();
+}
+*/
+
+// Example 2
+
+const dates = ["2024-1-10", "2025-2-0", "2026-4-12"];
+const formattedDates = dates.map(formatDates);
+
+console.log(formattedDates);
+
+function formatDates(element) {
+  const parts = element.split("-");
+  return `${parts[1]}/${parts[2]}/${parts[0]}`;
 }
