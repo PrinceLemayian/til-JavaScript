8d3c806c29762a63fb9b84d13fb5c312ba504cc2
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2025-11-03 02:31:59 +0300

    Added a new example for filterMethod

--- callBacks/filterMethod.js
+++ callBacks/filterMethod.js
@@ -1,3 +1,13 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const evenNumbers = numbers.filter((num) => num % 2 === 0);
-console.log(evenNumbers);
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+let evenNums = numbers.filter(isEven);
+let oddNums = numbers.filter(isOdd);
+console.log(evenNums);
+console.log(oddNums);
+
+function isEven(element) {
+  return element % 2 === 0;
+}
+
+function isOdd(element) {
+  return element % 2 !== 0;
+}
