5239a22099e04fd182f33b180c992b5f679d3dea
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2025-11-03 04:12:43 +0300

    Added prime number summing function

---
+++ callBacks/primeNumberSum.js
@@ -0,0 +1,20 @@
+function isPrime(n) {
+  if (n < 2) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+
+function sumPrimes(num) {
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+// Example test
+console.log(sumPrimes(10)); // 17
