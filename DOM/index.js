07046ccf089d8362840a7e93762f078603aaaddb
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2025-11-07 23:48:10 +0300

    Added some stuff

--- DOM/index.js
+++ DOM/index.js
@@ -1,5 +1,6 @@
 const container = document.getElementById("container");
 console.log(container);
+// Additional sections
 const sectionEl = document.querySelector("section");
 console.log(sectionEl);
 const highlightEl = document.querySelector(".highlight");
