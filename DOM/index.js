e9a9591976caff9de8325a9ad5498a4639e338c4
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2025-11-09 23:48:03 +0300

    Added some comments

--- DOM/index.js
+++ DOM/index.js
@@ -1,6 +1,7 @@
+// Examples for getelementbyId() and querySelector
+
 const container = document.getElementById("container");
 console.log(container);
-// Additional sections
 const sectionEl = document.querySelector("section");
 console.log(sectionEl);
 const highlightEl = document.querySelector(".highlight");
