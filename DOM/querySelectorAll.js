75d04f1f568fc30f746c494a507d78732145d561
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2025-11-12 22:41:48 +0300

    Added an example for querySelectorAll() method

---
+++ DOM/querySelectorAll.js
@@ -0,0 +1,8 @@
+// document.querySelectorAll(selectors);
+const matches = document.querySelectorAll("ul.ingredients li");
+
+for (let i = 0; i < matches.length; i++) {
+  console.log(matches[i]);
+}
+console.log(matches);
+console.log(matches[0]);
