b65bd8ad302c008cee550ad4ceea7f07ed019f55
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2025-11-12 23:49:03 +0300

    Added an example for the createElement() method

--- DOM/innerHTML.js
+++ DOM/innerHTML.js
@@ -1,3 +1,9 @@
 const container = document.getElementById("container");
 
 container.innerHTML = "<ul><li>Cheese</li><li>Tomato</li></ul>";
+
+// Creating a new node using the createELement() method
+const img = document.createElement("img");
+img.src = "https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg";
+img.alt = "A slice of lasagna on a plate.";
+container.appendChild(img);
