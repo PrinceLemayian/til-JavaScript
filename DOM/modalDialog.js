cdd4b652806798917045195a212f0934a6e62477
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2025-11-17 20:30:17 +0300

    Added examples for creating and closing a modal dialog

--- DOM/modalDialog.js
+++ DOM/modalDialog.js
@@ -1,2 +1,18 @@
 const dialog = document.getElementById("modal");
-dialog.showModal();
+const openButton = document.getElementById("open-modal-btn");
+const closeButton = document.getElementById("close-modal-btn");
+
+openButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+// If you needed to show a dialog while still allowing interaction with content outside of the dialog, you can use the show() method
+
+/* openButton.addEventListener("click", () => {
+  dialog.show();
+}); */
+
+// To close a modal, you can add a button to the modal inside the dialog element and use the close() method
