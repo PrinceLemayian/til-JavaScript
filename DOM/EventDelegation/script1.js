2c07cce86ae55bd6182454ac1528b4b1533a38b1
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2025-11-22 00:34:47 +0300

    Added an example for event bubbling and event delegation

--- DOM/EventDelegation/script1.js
+++ DOM/EventDelegation/script1.js
@@ -1,6 +1,5 @@
 const p = document.querySelector("p");
 const span = document.querySelector("span");
-p.addEventListener("click", (event) => {});
-span.addEventListener("click", (event) => {
+p.addEventListener("click", (event) => {
   event.target.style.color = "red";
 });
