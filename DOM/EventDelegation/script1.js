4249ab1a79247bd0a3d3d03536896bcb58910932
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2025-11-21 23:51:12 +0300

    Added an example for changing styles

---
+++ DOM/EventDelegation/script1.js
@@ -0,0 +1,6 @@
+const p = document.querySelector("p");
+const span = document.querySelector("span");
+p.addEventListener("click", (event) => {});
+span.addEventListener("click", (event) => {
+  event.target.style.color = "red";
+});
