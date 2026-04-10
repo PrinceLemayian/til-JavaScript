702ce21f8fb4f3b768d11c50b09ac1e9b92f6c60
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-11 02:24:21 +0300

    add examples for the logical assignment operators

---
+++ logicalAssignmentOperators/logicalAssignOperators.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const rest1 = {
+  name: "Capri",
+  numGuests: 20,
+};
+
+const rest2 = {
+  name: "La Piazza",
+  owner: "Giovanni Rossi",
+};
+
+// OR assignment operator
+
+// long method
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// OR assignment operator
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// Logical Nullish Assignment Operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && "<ANONYMOUS>";
+// rest2.owner = rest2.owner && "<ANONYMOUS>";
+rest1.owner &&= "<ANONYMOUS>";
+rest2.owner &&= "<ANONYMOUS>";
+
+console.log(rest1);
+console.log(rest2);
