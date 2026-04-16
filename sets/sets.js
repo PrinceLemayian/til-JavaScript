58d6bec1e47321a53822389aea107b9c1af0e775
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-16 23:48:08 +0300

    add examples for sets in javascript

---
+++ sets/sets.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const ordersSet = new Set([
+  "Pasta",
+  "Pizza",
+  "Pizza",
+  "Risotto",
+  "Pasta",
+  "Pizza",
+]);
+console.log(ordersSet);
+
+console.log(new Set("Lemayian"));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has("Pizza"));
+console.log(ordersSet.has("Bread"));
+ordersSet.add("Garlic Bread");
+ordersSet.add("Garlic Bread");
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// Example
+const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+
+console.log(
+  new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size,
+);
+
+console.log(new Set("princelemayian").size);
