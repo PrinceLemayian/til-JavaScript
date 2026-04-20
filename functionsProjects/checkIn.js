b99d086e831de6ea7408f6d20a9089c70825c620
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-20 15:20:37 +0300

    add example for value and reference argument passing

--- functionsProjects/checkIn.js
+++ functionsProjects/checkIn.js
@@ -1,27 +1,25 @@
-'use strict';
+"use strict";
 
-const flight = 'LH234';
+const flight = "LH234";
 const jonas = {
-    name: 'Jonas Schmedtmann',
-    passport: 2384793740237
-}
+  name: "Jonas Schmedtmann",
+  passport: 2384793740237,
+};
 
-const checkIn - function(flightNum, passenger) {
-    flightNum = 'LH898';
-    passenger.name = 'Mr. ' + passenger.name;
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH898";
+  passenger.name = "Mr. " + passenger.name;
 
-    if (passenger.passport === 2384793740237) {
-        alert('Checked in');
-    } else {
-        alert('Wrong Passport!');
-    }
-}
+  if (passenger.passport === 2384793740237) {
+    alert("Checked in");
+  } else {
+    alert("Wrong Passport!");
+  }
+};
 
-
-const newPassport = function(person) {
-    person.passport = Math.trunc(Math.random() * 1000000000)
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
 };
 
 newPassport(jonas);
 checkIn(flight, jonas);
-
