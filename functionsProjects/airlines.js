9c207245b2e19ddfdfbaa140bb5162045c692341
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-22 01:18:17 +0300

    add examples for the call and apply method

--- functionsProjects/airlines.js
+++ functionsProjects/airlines.js
@@ -8,8 +8,27 @@ const lufthansa = {
     console.log(
       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`,
     );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
   },
 };
 
 lufthansa.book(239, "Thorfinn Karlsefni");
 lufthansa.book(542, "Harvey Specter");
+console.log(lufthansa);
+
+const eurowings = {
+  airline: "Lufthansa",
+  iataCode: "LH",
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// this does not work
+// book(23, 'Clare Underwood');
+
+book.call(eurowings, 23, "Clare Underwood");
+console.log(eurowings);
+
+book.call(lufthansa, 239, "Barry Allen");
+console.log(lufthansa);
