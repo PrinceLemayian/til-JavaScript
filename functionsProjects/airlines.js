1789dabff4c02de503f8d1817849b1b12bb38509
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-22 02:25:32 +0300

    add example for bind method

--- functionsProjects/airlines.js
+++ functionsProjects/airlines.js
@@ -27,8 +27,50 @@ const book = lufthansa.book;
 // this does not work
 // book(23, 'Clare Underwood');
 
+// Call Method
 book.call(eurowings, 23, "Clare Underwood");
-console.log(eurowings);
 
 book.call(lufthansa, 239, "Barry Allen");
 console.log(lufthansa);
+
+const swiss = {
+  airline: "Swiss Air Lines",
+  iataCode: "SW",
+  bookings: [],
+};
+
+book.call(swiss, 834, "John Wick");
+console.log(swiss);
+
+// Apply method
+const flightData = [462, "Dr. No"];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
+
+// Bind method
+// book.call(eurowings, 23, "Clare Underwood");
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, "Billy Baker");
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23("Grace Cahill");
+bookEW23("Natalie Kabra");
+console.log(eurowings);
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+document
+  .querySelector(".buy")
+  .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
