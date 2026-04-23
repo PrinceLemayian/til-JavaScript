8c5ee123dcdd6f0e57ca232622cea4445018049b
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-23 03:27:15 +0300

    add example for closures

---
+++ closures/closureExamples.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
