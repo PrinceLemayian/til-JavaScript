d717f91d59bfeda78d4dfb92b22e7fcd46245958
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-23 03:37:28 +0300

    add example for closures

---
+++ closures/boarding.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
