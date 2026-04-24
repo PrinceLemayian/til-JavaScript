3d78ffe90fcab385c1e9185553c8c5c9d2d497a8
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-25 02:10:33 +0300

    add examples for using slice method

---
+++ arraysProjects/slice.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let arr = ["a", "b", "c", "d", "e"];
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log(arr.slice(...arr));
