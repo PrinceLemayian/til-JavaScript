cac799a7519f812d79e7b1fa36d95a25e1c93f96
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-29 01:34:44 +0300

    add example for maps looping with forEach

---
+++ maps/looping.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const currencies = new Map([
+  ["USD", "United States Dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
