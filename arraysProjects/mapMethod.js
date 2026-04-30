d7e4af70733beaf3c3f7f9bcf59e35005203bd18
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-04-30 23:37:21 +0300

    add more examples for map method

--- arraysProjects/mapMethod.js
+++ arraysProjects/mapMethod.js
@@ -22,6 +22,24 @@ const movementsUsd = movements.map((mov) => mov * eurToUsd);
 const movementsUsdFor = [];
 for (const mov of movements) movementsUsdFor.push(mov * eurToUsd);
 
+/*
+ 
+const movementsDescription = movements.map((mov, i) => {
+  if (mov > 0) {
+    return `Movement ${i + 1}: You deposited ${mov}`;
+  } else {
+    return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
+  }
+});
+
+*/
+
+const movementsDescription = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(mov)}`,
+);
+
 console.log(movementsUsdFor);
 console.log(movements);
 console.log(movementsUsd);
+console.log(movementsDescription);
