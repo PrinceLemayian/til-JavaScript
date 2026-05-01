4ddd202140c0a636ee6f6003a643e86d8bb7148f
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-05-02 02:40:15 +0300

    add example for reduce method

---
+++ arraysProjects/reduceMethod.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const balance = movements.reduce(function (acc, curr, i) {
+  console.log(`Iteration ${i}: ${acc}`);
+  return acc + curr;
+}, 0);
+console.log(balance);
+
+let accBalance = 0;
+for (const mov of movements) accBalance += mov;
+console.log(accBalance);
+
+const balances = movements.reduce((acc, curr) => acc + curr);
+
+console.log(balances);
