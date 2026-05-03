65e94900208ea03138e534f832c99af1d501875b
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-05-03 18:12:55 +0300

    add example for some method

--- arraysProjects/someMethod.js
+++ arraysProjects/someMethod.js
@@ -3,7 +3,7 @@
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // Equality
-console.log(movements.include(-130));
+console.log(movements.includes(-130));
 
 // Condition
 console.log(movements.some((mov) => mov === -130));
