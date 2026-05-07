b4c9e30c723228d269f6a4281d182cad5afaf53c
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-05-07 23:13:13 +0300

    add example for number conversion and parsing

---
+++ numbers/numbers.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Conversion
+console.log(Number("33"));
+console.log(+"33");
+
+// Parsing
+console.log(Number.parseInt("33px", 10));
+console.log(Number.parseInt("0011px", 2));
+console.log(Number.parseFloat("2.5rem"));
