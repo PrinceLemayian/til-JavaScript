ac9be66b3e46ee2c5a3fd3aa48ea1f66ed3930da
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-05-12 23:08:29 +0300

    add examples to check if a value is NaN or a Number

--- numbers/numbers.js
+++ numbers/numbers.js
@@ -8,3 +8,14 @@ console.log(+"33");
 console.log(Number.parseInt("33px", 10));
 console.log(Number.parseInt("0011px", 2));
 console.log(Number.parseFloat("2.5rem"));
+
+console.log(Number.isNaN(20));
+console.log(Number.isNaN("20"));
+console.log(Number.isNaN(+"20X"));
+console.log(Number.isNaN(23 / 0));
+
+// Best way of checking if a value is a number
+console.log(Number.isFinite(20));
+console.log(Number.isFinite("20"));
+console.log(Number.isFinite(+"20X"));
+console.log(Number.isFinite(23 / 0));
