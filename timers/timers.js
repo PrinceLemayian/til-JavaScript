798538d50bbe37d3395e80def75be6b59a831d68
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-05-17 03:06:28 +0300

    add examples for timers

---
+++ timers/timers.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const ingredients = ["olives", "spinach"];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  3000,
+  ...ingredients,
+);
+
+console.log("Waiting...");
+if (ingredients.includes("spinach")) clearTimeout(pizzaTimer);
