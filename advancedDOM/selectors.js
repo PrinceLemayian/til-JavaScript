4bfd459acc131484b755302552b36ba231b4d3a8
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-05-19 23:20:05 +0300

    add example for DOM selectors

---
+++ advancedDOM/selectors.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector(".button");
+const allBtns = document.querySelectorAll(".button");
+console.log(allBtns);
+
+document.getElementById("buy");
