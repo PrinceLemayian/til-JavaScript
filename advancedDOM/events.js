8584319985263c144bd943d3a94e0a33ff53e80a
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-05-26 20:47:40 +0300

    add examples for events and event handlers

---
+++ advancedDOM/events.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const h1 = document.querySelector("h1");
+
+const alertH1 = function (e) {
+  alert("addEventListener: Great! You are reading the heading :D");
+
+  h1.removeEventListener("mouseenter", alertH1);
+};
+
+h1.addEventListener("mouseenter", alertH1);
+
+setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
+
+// Old way
+// h1.onmouseenter = function (e) {
+//   alert("onmouseenter: Great! You are reading the heading :D");
+// };
