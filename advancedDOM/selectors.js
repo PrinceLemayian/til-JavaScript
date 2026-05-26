0b795490ed8e498f093e2cf4238d002b9bfa87b3
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-05-26 18:30:32 +0300

    add examples for selecting, creating and deleting elements in the DOM

--- advancedDOM/selectors.js
+++ advancedDOM/selectors.js
@@ -4,8 +4,36 @@ console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-document.querySelector(".button");
-const allBtns = document.querySelectorAll(".button");
-console.log(allBtns);
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
+console.log(allSections);
 
-document.getElementById("buy");
+document.getElementById("section--1");
+const allButtons = document.getElementByTagName("button");
+console.log(allButtons);
+
+console.log(document.getElementByClassName("btn"));
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+// message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+console.log(message);
+header.prepend(message);
+// header.append(message);
+header.append(message.cloneNode(true));
+
+header.before(message);
+header.after(message);
+
+// Delete elements
+document
+  .querySelector("btn--close-cookie")
+  .addEventListener("click", function () {
+    // message.remove();
+    message.parentElement.removeChild(message);
+  });
