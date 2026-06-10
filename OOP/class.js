ab30955ceff2808ae4a706c6f87ca141cfdd6044
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-06-10 22:36:57 +0300

    add example for using setters & getters

--- OOP/class.js
+++ OOP/class.js
@@ -14,11 +14,28 @@ class PersonCl {
   greet() {
     console.log(`Hey ${this.firstName}`);
   }
+
+  get age() {
+    return 2026 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(" ")) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const mercy = new PersonCl("Mercy", 1996);
+const mercy = new PersonCl("Mercy Kibz", 1996);
 console.log(mercy);
 mercy.calcAge();
+console.log(mercy.age);
 
 console.log(mercy.__proto__ === PersonCl.prototype);
 mercy.greet();
+
+const thorfinn = new PersonCl("Thorfinn Karlsefni", 1987);
