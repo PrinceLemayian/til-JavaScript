24e9a25d265f6926295637cc44653c4a4e6ef71b
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-06-11 23:41:43 +0300

    add example for inheritance using constructor functions

---
+++ OOP/constructorInheritance.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2026 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student("Mike", 2002, "Computer Science");
+mike.introduce();
