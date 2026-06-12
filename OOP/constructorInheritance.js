8328922554a1496b57a0e8640c9458bb30479ab3
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-06-12 23:40:56 +0300

    add example for inheritance

--- OOP/constructorInheritance.js
+++ OOP/constructorInheritance.js
@@ -34,3 +34,41 @@ console.log(mike instanceof Object);
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
+
+// Parent constructor function
+function Animal(name) {
+  this.name = name;
+}
+
+// Parent method
+Animal.prototype.speak = function () {
+  console.log(`${this.name} makes a sound.`);
+};
+
+// Child constructor function
+function Dog(name, breed) {
+  // Inherit properties from Animal
+  Animal.call(this, name);
+
+  this.breed = breed;
+}
+
+// Inherit methods from Animal
+Dog.prototype = Object.create(Animal.prototype);
+
+// Reset constructor property
+Dog.prototype.constructor = Dog;
+
+// Child-specific method
+Dog.prototype.bark = function () {
+  console.log(`${this.name} barks!`);
+};
+
+// Create an instance
+const dog1 = new Dog("Buddy", "Golden Retriever");
+
+dog1.speak(); // Buddy makes a sound.
+dog1.bark(); // Buddy barks!
+
+console.log(dog1 instanceof Dog); // true
+console.log(dog1 instanceof Animal); // true
