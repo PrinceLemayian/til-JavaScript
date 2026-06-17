2522f3f171110c5d2b43d0ca0413afd39b8f5678
PrinceLemayian <189862853+PrinceLemayian@users.noreply.github.com>
2026-06-17 19:07:03 +0300

    add example for inheritance using objects

---
+++ OOP/objectInheritance.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const mercy = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+studentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init("Jay", 2010, "Computer Science");
+jay.introduce();
+jay.calcAge();
